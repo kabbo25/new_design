624c7747bed997f864f4fda1b459f24efb25e880
kabbo25 <kabbo25@gmail.com>
2024-10-28 14:55:45 +0600

    added two working status card

--- lib/features/finish_working/view/pages/finish_working_page.dart
+++ lib/features/finish_working/view/pages/finish_working_page.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:new_design/features/finish_working/model/working_status.dart';
 import 'package:new_design/features/finish_working/view/widgets/network_profile_bar.dart';
+import 'package:new_design/features/finish_working/view/widgets/working_status_card.dart';
 import 'package:new_design/features/finish_working/viewmodel/finish_working_view_model.dart';
 import 'package:new_design/features/start_page/view/widgets/bottom_navigation_section.dart';
 import 'package:provider/provider.dart';
 
 class FinishWorkingPage extends StatelessWidget {
-  const FinishWorkingPage({super.key});
+  final WorkMode workMode;
+
+  const FinishWorkingPage({
+    super.key,
+    required this.workMode,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => FinishWorkingViewModel(),
+      create: (_) => FinishWorkingViewModel(workMode: workMode),
       child: const FinishWorkingView(),
     );
   }
@@ -22,32 +29,40 @@ class FinishWorkingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<FinishWorkingViewModel>();
-    return const Scaffold(
+
+    return Scaffold(
       body: Stack(
         children: [
           SafeArea(
             child: Padding(
-              padding: EdgeInsets.fromLTRB(20, 40, 20, 10),
+              padding: const EdgeInsets.fromLTRB(20, 40, 20, 10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   //network status bar with circle avatar
-                  NetworkProfileBar(),
-                  SizedBox(height: 20),
+                  const NetworkProfileBar(),
+                  const SizedBox(height: 20),
                   //start working card
+                  WorkingStatusCard(
+                    workingStatus: viewModel.workingStatus,
+                    onEdit: () => (),
+                  ),
                   //finished working card
+                  WorkingStatusCard(
+                    workingStatus: viewModel.workingStatus,
+                    onEdit: () => (),
+                  ),
                   //My locations card that show all the location through bottom modal
                   //Timer section
                   //if any note added, it will appear here
                   //add a note elevated button
-                  Stack(
+                  const Stack(
                     children: [
                       // Positioned.fill(
-                      //   child: BackgroundWidget(
-                      //       config: viewModel.backgroundConfig),
+                      // child: BackgroundWidget(
+                      // config: viewModel.backgroundConfig),
                       // ),
-
                       //seet you tommorrow text
                       //text button work again
                       BottomNavigationSection(),
